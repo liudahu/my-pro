f20102c188d0e8f9ba3e5afea3e62a0464325f9a
liudahu <174927228@qq.com>
2017-05-13 16:44:02 +0800

    创建cli脚手架

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="less" rel="stylesheet/less">
+  *{margin:0;padding: 0;}
+
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    main
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'main',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
